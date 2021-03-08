fb79b96b4d4aabe0f0239c567ecc6f95d86742b5
sptkl <caobaiyue@gmail.com>
2021-03-08 09:05:01 -0500

    adding url parameters

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.library import Library
 from src.utils import emojis
 import random
 
+# Set page configuration
 st.set_page_config(
     page_title='Data Library',
     page_icon="📚",
@@ -11,24 +12,47 @@ st.set_page_config(
 
 )
 
+# Logo
+st.sidebar.markdown(
+    """
+    <div stule="margin-left: auto; margin-right: auto;">
+    <img style='width:40%; margin: 0 auto 2rem auto;display:block;'
+        src="https://raw.githubusercontent.com/NYCPlanning/logo/master/dcp_logo_772.png">
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Initiate library class to get a list of available datasets
 library=Library()
 datasets=library.datasets
-name=st.sidebar.selectbox('Select a Dataset', datasets, index=datasets.index('doe_lcgms'))
 
+# Set dataset name as query param
+query_params = st.experimental_get_query_params()
+default = query_params["dataset"][0] if "dataset" in query_params else 'doe_lcgms'
+name=st.sidebar.selectbox('Select a Dataset', datasets, index=datasets.index(default))
+st.experimental_set_query_params(dataset=name)
+
+# Initiate dataset class
 dataset=Dataset(name)
 config = dataset.get_config_by_version('latest')
 st.sidebar.write(config)
 
+# List all versions and find latest version
 versions = dataset.list_versions
 latest_version = dataset.latest_version
 other_versions = [v for v in versions if v not in ['latest', latest_version]]
 other_versions.sort(reverse=True)
 sorted_versions = ['latest', latest_version] + other_versions
 
+# Layout for each dataset
 st.markdown(f"# {random.choice(emojis)} {name}")
 st.markdown(config['dataset']['info']['description'])
 st.markdown(f"[more info]({config['dataset']['info']['url']})")
 def create_download_link_markdown(version) -> str:
+    """
+    Create a list of hyperlink markdowns for the download urls
+    """
     files=dataset.list_files_by_version(version)
     mk = []
     for f in files:
@@ -37,6 +61,7 @@ def create_download_link_markdown(version) -> str:
         mk.append(f'[{filename}]({url})')
     return mk
 
+# For each version, render a download content
 for v in sorted_versions:
     mk = create_download_link_markdown(v)
     if v == 'latest': 
